a59f7eaa88147e50a979bb57249d3303020e2865
YAAOO <393533486>
2022-03-06 21:43:01 +0800

    first page

---
+++ pages/index.html.js
@@ -0,0 +1,25 @@
+export const data = {
+  "key": "v-8daa1a0e",
+  "path": "/",
+  "title": "",
+  "lang": "zh-CN",
+  "frontmatter": {},
+  "excerpt": "",
+  "headers": [
+    {
+      "level": 2,
+      "title": "Wellcom to my Blog",
+      "slug": "wellcom-to-my-blog",
+      "children": [
+        {
+          "level": 3,
+          "title": "this is my first blog",
+          "slug": "this-is-my-first-blog",
+          "children": []
+        }
+      ]
+    }
+  ],
+  "git": {},
+  "filePathRelative": "README.md"
+}
